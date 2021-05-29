10747a1bfcdcef27f3810acfcb9638e4ddbf777b
olegario-gu <gustavolegario.sousa@gmail.com>
2021-05-29 00:51:49 -0300

    codigo inicial

--- client-pedra-papel-tesoura/src/Client.java
+++ client-pedra-papel-tesoura/src/Client.java
@@ -1,4 +1,94 @@
+/**
+ * A classe Client cria uma conexão com o servidor Server na porta padrão 1337.
+ * Espera que o usuário insira uma string com o teclado referente a sua escolha:
+ * Pedra, Papel ou Tesoura!
+ * 
+ * Envia a string para o servidor por meio de um protocolo TCP. 
+ * Espera por uma resposta do Servidor. 
+ * Imprime a mensagem recebida do servidor. 
+ * Fecha a conexão.
+ *
+ * @author Gustavo Olegário, Bruno Costa, Matheus Soares e Eduardo Gomes <https://github.com/olegario-gu/pedra-papel-tesoura>
+ * @version 1.0
+ * 
+ */
 
-public class Client {
+import java.io.*;
+import java.net.*;
 
+class Client {
+
+	/**
+	 * O numero padrão(default) do host
+	 * 
+	 * @var string
+	 */
+	private static String host = "localhost";
+
+	/**
+	 * A porta
+	 * 
+	 * @var integer
+	 */
+	private static Integer port = 1337;
+
+	/**
+	 * A versão da classe Client
+	 * 
+	 * @var double
+	 */
+	private static Double numVersao = 1.0;
+
+	/**
+	 * Mensagem de boas vindas do Jogo
+	 * 
+	 * @var string
+	 */
+	private static String msgBoasVindas = "--- Bem-Vindo ao Jokenpo V. " + numVersao + " --- \n";
+
+	/**
+	 * Mensagem de regras do jogo
+	 * 
+	 * @var string
+	 */
+	private static String msgRegras = "\nRegras: \n-> Pedra ganha da Tesoura \n-> Tesoura ganha do Papel \n-> Papel ganha da Pedra\n";
+
+	public static void main(String args[]) throws Exception {
+
+		String entrada = "";
+		String resposta;
+
+		System.out.println(Client.msgBoasVindas);
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket(Client.host, Client.port);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		do {
+
+			if (entrada.equals("REGRAS")) {
+				System.out.println(Client.msgRegras);
+			}
+
+			// Solicita ao usuário a seleção de pedra, papel ou tesoura ...
+			System.out.println("Comece o jogo escrevendo qual sua escolha: \n Pedra || Papel || Tessoura");
+			System.out.print("ou digite \"regras\" para ver as regras do jogo: ");
+			entrada = inFromUser.readLine();
+			entrada = entrada.toUpperCase();
+
+		} while (!entrada.equals("PEDRA") && !entrada.equals("PAPEL") && !entrada.equals("TESOURA"));
+
+		// Transmite a entrada para o servidor e fornece o feedback adequado do jogador
+		outToServer.writeBytes(entrada + "\n");
+		System.out.println("\nSua jogada (" + entrada + ") foi salva com sucesso no servidor. \nAguarde o resultado ...");
+
+		// Pega a resposta do servidor e imprime ao jogador
+		resposta = inFromServer.readLine();
+		System.out.println("Resultado: " + resposta);
+
+		// Fecha o Socket
+		clientSocket.close();
+
+	}
 }
--- server-pedra-papel-tesoura/src/Server.java
+++ server-pedra-papel-tesoura/src/Server.java
@@ -1,4 +1,201 @@
+/**
+ * A classe do servidor aguarda a conexão dos dois clientes client_1 
+ * e client_2 (em qualquer ordem) na porta 1337. Recebe um caractere
+ * de client_1 e um caractere de client_2 (em qualquer ordem) e
+ * calcula o vencedor do jogo com base em um conjunto de regras.
+ * Depois de enviar uma mensagem correspondente a cada cliente o 
+ * servidor espera novamente por dois clientes para se conectar.
+ * 
+ * @author Gustavo Olegário, Bruno Costa, Matheus Soares e Eduardo Gomes <https://github.com/olegario-gu/pedra-papel-tesoura>
+ * @version 1.0
+ * 
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class Server {
 
+	/**
+	 * O numero padrão(default) da porta.
+	 * 
+	 * @var integer
+	 * 
+	 */
+	private static Integer port = 1337;
+
+	/**
+	 * O numero da versão do jogo
+	 * 
+	 * @var integer
+	 * 
+	 */
+	private static Double numVersao = 1.0;
+
+	/**
+	 * Mensagem de boas vindas do Servidor
+	 * 
+	 * @var string
+	 * 
+	 */
+	private static String msgBoasVindas = "--- Bem-vindo ao Servidor do Jokenpo V. " + numVersao + " --- \n";
+
+	/**
+	 * A função recebe um inteiro x como entrada e retorna o valor booleano
+	 * verdadeiro se a entrada for estritamente maior que 0 e menor ou igual a
+	 * 65535.
+	 * 
+	 * @param integer
+	 *            x
+	 * @return boolean
+	 */
+	private static boolean portaValida(Integer x) {
+		return x >= 1 && x <= 65535 ? true : false;
+	}
+
+	/**
+	 * A função solicita que o usuário escolha um número de porta específico ou pressione
+	 * "0" para continuar com a configuração padrão (Server.port).
+	 * 
+	 * O número inteiro retornado estritamente maior que 0 e menor ou igual a
+	 * 65535.
+	 * 
+	 * @return integer
+	 */
+	private static int getPort() {
+
+		Integer input;
+
+		Scanner entrada = new Scanner(System.in);
+
+		do {
+			System.out.print("Selecione uma porta inserindo um valor inteiro entre 1 e 65535 ou\n");
+			System.out.print("A porta padrão é a 1337, caso escolha outra, será necessário trocar manualmente a porta do Client.java\n");
+			System.out.print("insira \"0\" para continuar com a configuração padrão (" + Server.port + "): ");
+			input = entrada.nextInt();
+
+		} while (input != 0 && !Server.portaValida(input));
+
+		entrada.close();
+
+		return input == 0 ? Server.port : input;
+	}
+
+	public static void main(String args[]) throws Exception {
+
+		String resClient_1 = "";
+		String resClient_2 = "";
+		String inputClient_1;
+		String inputClient_2;
+
+		// Imprime a mensagem de boas vindas do Server
+		System.out.println(Server.msgBoasVindas);
+
+		// Setta a porta escolhida
+		Server.port = Server.getPort();
+
+		// Cria um novo server socket & imprime a mensagem de status de funcionamento
+		ServerSocket welcomeSocket = new ServerSocket(Server.port);
+		System.out.println("\nOk, estamos funcionando na porta " + welcomeSocket.getLocalPort() + " ...");
+
+		while (!welcomeSocket.isClosed()) {
+
+			// Jogador 1
+			Socket client_1 = welcomeSocket.accept();
+			if (client_1.isConnected()) {
+				System.out.println("\nJogador 1 (" + (client_1.getLocalAddress().toString()).substring(1) + ":"
+						+ client_1.getLocalPort() + ") entrou no servidor ... esperando o segundo jogador ...");
+			}
+			DataOutputStream outClient_1 = new DataOutputStream(client_1.getOutputStream());
+			BufferedReader inClient_1 = new BufferedReader(new InputStreamReader(client_1.getInputStream()));
+
+			// Jogador 2
+			Socket client_2 = welcomeSocket.accept();
+			if (client_2.isConnected()) {
+				System.out.println("Jogador 2 (" + (client_2.getLocalAddress().toString()).substring(1) + ":"
+						+ client_1.getLocalPort() + ") entrou no servidor ... Vamos começar!");
+			}
+			DataOutputStream outClient_2 = new DataOutputStream(client_2.getOutputStream());
+			BufferedReader inClient_2 = new BufferedReader(new InputStreamReader(client_2.getInputStream()));
+
+			// Recebe o input dos dois jogadores
+			inputClient_1 = inClient_1.readLine();
+			inputClient_2 = inClient_2.readLine();
+
+			/**
+			 * Se os inputs de C1 e C2 forem iguais, o
+			 * servidor envia de volta para ambos os clientes a string "Empate!".
+			 */
+			if (inputClient_1.equals(inputClient_2)) {
+				resClient_1 = "Empate!";
+				resClient_2 = "Empate!";
+				System.out.println("Deu empate!");
+			}
+			/**
+			 * Se o servidor receber 'PEDRA' do C1 e 'TESOURA' do C2, 
+			 * enviamos a string "Você Ganhou!" ao C1 e "Você Perdeu!" ao C2.
+			 */
+			else if (inputClient_1.equals("PEDRA") && inputClient_2.equals("TESOURA")) {
+				resClient_1 = "Você Ganhou!";
+				resClient_2 = "Você Perdeu!";
+				System.out.println("Jogador 1 ganhou!");
+
+			}
+			/**
+			 * Se o servidor receber 'TESOURA' do C1 e 'PEDRA' do C2,
+			 * enviamos a string "Você Ganhou!" ao C2 e "Você Perdeu!" ao C1.
+			 */
+			else if (inputClient_1.equals("TESOURA") && inputClient_2.equals("PEDRA")) {
+				resClient_1 = "Você Perdeu!";
+				resClient_2 = "Você Ganhou!";
+				System.out.println("Jogador 2 ganhou!");
+			}
+			/**
+			 * Se o servidor receber 'PEDRA' do C1 e 'PAPEL' do C2,
+			 * enviamos a string "Você Ganhou!" ao C2 e "Você Perdeu!" ao C1.
+			 */
+			else if (inputClient_1.equals("PEDRA") && inputClient_2.equals("PAPEL")) {
+				resClient_1 = "Você Perdeu!";
+				resClient_2 = "Você Ganhou!";
+				System.out.println("Jogador 2 ganhou!");
+			}
+			/**
+			 * Se o servidor receber 'PAPEL' do C1 e 'PEDRA' do C2,
+			 * enviamos a string "Você Ganhou!" ao C1 e "Você Perdeu!" ao C2.
+			 */
+			else if (inputClient_1.equals("PAPEL") && inputClient_2.equals("PEDRA")) {
+				resClient_1 = "Você Ganhou!";
+				resClient_2 = "Você Perdeu!";
+				System.out.println("Jogador 1 ganhou!");
+			}
+			/**
+			 * Se o servidor receber 'TESOURA' do C1 e 'PAPEL' do C2,
+			 * enviamos a string "Você Ganhou!" ao C1 e "Você Perdeu!" ao C2.
+			 */
+			else if (inputClient_1.equals("TESOURA") && inputClient_2.equals("PAPEL")) {
+				resClient_1 = "Você Ganhou!";
+				resClient_2 = "Você Perdeu!";
+				System.out.println("Jogador 1 ganhou!");
+			}
+			/**
+			 * Se o servidor receber 'PAPEL' do C1 e 'TESOURA' do C2,
+			 * enviamos a string "Você Ganhou!" ao C2 e "Você Perdeu!" ao C1.
+			 */
+			else if (inputClient_1.equals("PAPEL") && inputClient_2.equals("TESOURA")) {
+				resClient_1 = "Você Perdeu!";
+				resClient_2 = "Você Ganhou!";
+				System.out.println("Jogador 2 ganhou!");
+			}
+
+			// Send responses in uppercase and close sockets
+			outClient_1.writeBytes(resClient_1.toUpperCase());
+			outClient_2.writeBytes(resClient_2.toUpperCase());
+			client_1.close();
+			client_2.close();
+
+			System.out.println("\nEsperando por novos jogadores ...\n");
+
+		}
+	}
 }
